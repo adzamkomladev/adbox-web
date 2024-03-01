c45a3bf7f50ea6edf7fd573131f7503e8d7e04f2
Komla Adzam <adzamkomla.dev@gmail.com>
2024-03-01 00:31:29 +0000

    Add login

--- modules/auth/src/lib/login/login.page.ts
+++ modules/auth/src/lib/login/login.page.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { Form } from './models/form.model';
 
 @Component({
-  selector: 'adbox-login',
-  standalone: true,
-  imports: [CommonModule],
-  template: `<p>login works!</p>`,
-  styles: ``,
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './login.page.html',
+    styles: ``,
 })
-export class LoginPage {}
+export class LoginPage {
+    model: Form = {
+        email: '',
+        password: '',
+        rememberMe: false
+    };
+
+    loading = false;
+    message?: string | null;
+    success = false;
+
+    onSubmit() {
+        console.log(this.model);
+        this.success = true;
+    }
+}
--- modules/auth/src/lib/password/forgot/forgot.page.ts
+++ modules/auth/src/lib/password/forgot/forgot.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'adbox-forgot',
   standalone: true,
   imports: [CommonModule],
   template: `<p>forgot works!</p>`,
